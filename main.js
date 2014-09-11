19b7904a210cb65da525aadb541a52fdfda90fcf
Gustavo Henrique <gustavo@gustavohenrique.net>
2014-09-11 16:43:17 -0300

    some design adjustments

--- main.js
+++ main.js
@@ -1,44 +1,79 @@
 'use strict';
 
-var messages = {
-    found: {
-        text: 'Infelizmente seu e-mail est&aacute; na lista. Mude sua senha imediatamente.',
-        cls: 'ui red message'
+var config = {
+    url: 'http://localhost:3000/checkemail',
+    i18n: {
+        enUS : {
+            found: {
+                text: 'Sorry! Your account was compromised.',
+                cls: 'ui red message'
+            },
+            notFound: {
+                text: 'Keep calm, your account was not compromised.',
+                cls: 'ui green message'
+            },
+            validation: {
+                text: 'Invalid e-mail.',
+                cls: 'ui red message'
+            },
+            about: 'Nearly 5 Million Google Passwords Leaked on Russian Site.<br/>Check if your was compromised.'
+        },
+        ptBR : {
+            found: {
+                text: 'Infelizmente sua conta foi comprometida.',
+                cls: 'ui red message'
+            },
+            notFound: {
+                text: 'Fique tranquilo(a), sua conta n&atilde;o foi comprometida.',
+                cls: 'ui green message'
+            },
+            validation: {
+                text: 'E-mail inv&aacute;lido',
+                cls: 'ui red message'
+            },
+            about: 'Quase 5 milh&otilde;es de senhas de contas do Google vazaram num site Russo.<br/>Verifique se a sua foi comprometida.'
+        }
     },
-    notFound: {
-        text: 'Fique tranquilo(a), seu e-mail não está na lista',
-        cls: 'ui green message'
+    messages: {}
+};
+
+var app = {
+    showMessage: function (message) {
+        var div = $('#message');
+        div.removeClass(config.messages.found.cls);
+        div.removeClass(config.messages.notFound.cls);
+        div.addClass(message.cls);
+        div.html('<i class="close icon"></i><div class="header"></div>' + message.text);
     },
-    validation: {
-        text: 'E-mail inv&aacute;lido',
-        cls: 'ui red message'
-    }
+
+    isValid: function (email) {
+        if ($.trim(email).length === 0 || email === '') return false;
+        var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(email);
+    },
+
+    checkEmail: function () {
+        var $this = this,
+            email = $('#email').val();
+
+        if ($this.isValid(email)) {
+            $.post(URL, {'email': email})
+             .done(function (data) { $this.showMessage(config.messages.found); })
+             .fail(function (data) { $this.showMessage(config.messages.notFound); });
+        }
+        else {
+            $this.showMessage(config.messages.validation);
+        }
+        $('#email').focus();
+    },
+
+    load: function (language) {
+        config.messages = config.i18n[language];
+        $('#about').html(config.messages.about);
+    },
+
 };
 
-var showMessage = function (message) {
-    var div = $('#message');
-    div.removeClass(messages.found.cls);
-    div.removeClass(messages.notFound.cls);
-    div.addClass(message.cls);
-    div.html(message.text);
-};
-
-var isValid = function (email) {
-    if ($.trim(email).length === 0 || email === '') return false;
-    var filter = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return filter.test(email);
-};
-
-var checkemail = function () {
-    var email = $('#email').val();
-
-    if (isValid(email)) {
-        $.post('http://localhost:3000/checkemail', {'email': email})
-         .done(function (data) { showMessage(messages.found); })
-         .fail(function (data) { showMessage(messages.notFound); });
-    }
-    else {
-        showMessage(messages.validation);
-    }
-    $('#email').focus();
-};
+$(document).ready(function () {
+    app.load('ptBR');
+});
